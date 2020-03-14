a02aa254ac25b0d6e5b12d6dce62900757bf2d60
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-14 15:30:41 -0500

    [Feature #1] Core of _printf. Creates buffer, iterators, size pointer, handler. If find % call a function

---
+++ print_general.c
@@ -0,0 +1,44 @@
+#include "printf.h"
+/**
+ * _printf - clone of printf native function
+ * @format: string
+ * Return: #chars printed
+ */
+int _printf(const char * const format, ...)
+{
+	va_list vars;
+	char *buff;
+	int i = 0, j = 0, buff_size = 0, *size = &buff_size;
+
+	buff = malloc(2048);
+	if (!buff)
+		return (000);
+	*buff = '\0';
+
+	filter handler[] = {
+		{'c', print_c}, {'s', print_str}};
+
+	va_start(vars, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (j < 4)
+			{
+				if (format[i + 1] == handler[j].c)
+				{
+					buff = handler[j].f(vars, buff, size);
+					i++;
+				}
+				j++;
+			}
+		}
+		else
+			buff[buff_size++] = format[i];
+		i++;
+	}
+	buff_size = _strlen(buff);
+	print_buff(buff, buff_size);
+	return  (buff_size);
+}
