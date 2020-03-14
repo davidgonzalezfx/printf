b0699c011e771dee2cba816de1512507ae95fc34
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-14 15:27:27 -0500

    [Tool #1] _strlen function to get length of strings

---
+++ print_auxs.c
@@ -0,0 +1,15 @@
+#include "printf.h"
+/**
+ * _strlen - return length of string
+ * @s: char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int cnt;
+
+	for (cnt = 0; *(s + cnt) != '\0'; cnt++)
+	{
+	}
+	return (cnt);
+}
