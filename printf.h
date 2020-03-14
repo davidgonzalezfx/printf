0fcd62e9e485a09ec73ce3532dfccb2f8e787591
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-14 15:18:54 -0500

    [Header] Prototypes of all used functions

---
+++ printf.h
@@ -0,0 +1,32 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include "stdarg.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include <unistd.h>
+
+/**
+ * struct funs - functions
+ * @c: char
+ * @f: funs
+ */
+typedef struct funs
+{
+	char c;
+	char *(*f)();
+} filter;
+
+int _printf(const char *const format, ...);
+
+char *print_c(va_list vars, char *buff, int *buff_size);
+char *print_str(va_list vars, char *buff, int *buff_size);
+
+char *print_d(va_list vars, char *buff, int *buff_size);
+
+void print_buff(char *, int);
+
+char *str_concat(char *s1, char *s2);
+int _strlen(char *s);
+
+#endif
