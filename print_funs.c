f3d8a0ec1c0ca7ea2aa9ec039d63dcc2ff57b45e
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-15 12:39:42 -0500

    [Feature #2] Handling %i and %d . print_d function added

--- print_funs.c
+++ print_funs.c
@@ -1,5 +1,18 @@
-#include "printf.h"
-#include <stdlib.h>
+#include "holberton.h"
+/**
+ * hand - array of filters
+ * Return: filter *
+ */
+filter *hand()
+{
+	filter *hand = malloc(sizeof(filter) * 4);
+
+	hand[0].c = 'c', hand[0].f = print_c, hand[1].c = 's', hand[1].f = print_str;
+	hand[2].c = 'i', hand[2].f = print_d, hand[3].c = 'd', hand[3].f = print_d;
+
+	return (hand);
+}
+
 /**
  * print_c - add char to buffer
  * @vars: char to be added
@@ -25,6 +38,25 @@ char *print_c(va_list vars, char *buff, int *buff_size)
 char *print_str(va_list vars, char *buff, int *buff_size)
 {
 	char *str = va_arg(vars, char *);
+
+	for (; *str; str++)
+	{
+		buff[*buff_size] = *str;
+		*buff_size += 1;
+	}
+	return (buff);
+}
+/**
+ * print_d - print digits base 10
+ * @vars: char to be added
+ * @buff: buffer
+ * @buff_size: index of buffer
+ * Return: char * - buffer
+ */
+char *print_d(va_list vars, char *buff, int *buff_size)
+{
+	int num = va_arg(vars, int);
+	char *str = _itoa(num, 10);
 
 	for (; *str; str++)
 	{
